bd0d264d219c28c21a8e548dfce836f2d6e6459e
willjasen <willjasen@gmail.com>
2015-05-29 12:55:14 -0700

    removing shell calls, sourcing bashrc to proceed with script

--- setup.sh
+++ setup.sh
@@ -19,11 +19,10 @@ sudo apt-get -y install nodejs libpq-dev libmagickwand-dev libqt4-dev libqtwebki
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
- 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+source ~/.bashrc
+
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
  
 # Install Ruby from rbenv
