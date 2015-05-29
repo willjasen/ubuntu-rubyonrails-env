1c07794f94959aac372886b1f18343c56bc339e2
willjasen <willjasen@gmail.com>
2015-05-29 15:07:44 -0700

    executing eval as well as placing into script

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 export PATH="$HOME/.rbenv/bin:$PATH"
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+eval "$(rbenv init -)"
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
