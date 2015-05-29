32da0206e53b8a373cd4e3a583a7860145f91a9f
willjasen <willjasen@gmail.com>
2015-05-29 12:38:51 -0700

    minor changes

--- setup.sh
+++ setup.sh
@@ -22,15 +22,17 @@ echo 'eval "$(rbenv init -)"' >> ~/.bashrc
  
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
- 
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
  
 # Install Ruby from rbenv
 rbenv install 2.2.2
 rbenv global 2.2.2
 ruby -v
- 
+
+# Tell Ruby not to download documentation
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+
+# Install bundler
 gem install bundler
  
 # Install Rails
