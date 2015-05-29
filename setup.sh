1cb5013417190973f5a5d075fca8541575aa3eff
willjasen <willjasen@gmail.com>
2015-05-29 13:00:54 -0700

    replacing source with .

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+. ~/.bashrc
 
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
  
